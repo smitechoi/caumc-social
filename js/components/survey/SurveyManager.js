e05d2a3be4a6911b39e7af05137eb74fa48ddab8
smitechoi <delf@cau.ac.kr>
2025-06-13 15:20:05 +0900

    language

--- js/components/survey/SurveyManager.js
+++ js/components/survey/SurveyManager.js
@@ -164,8 +164,8 @@ export class SurveyManager {
       this.render();
       
     } catch (error) {
-      console.error('Scale 저장 오류:', error);
-      alert('저장 중 오류가 발생했습니다. 다시 시도해주세요.');
+      console.error('Scale Saving Error:', error);
+      alert('Please Retry');
     }
   }
 
